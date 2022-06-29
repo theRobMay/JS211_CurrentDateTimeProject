608f79c0015e915231b5c2378359381a4c454a5d
theRobMay <robbie.maysent@gmail.com>
2022-06-28 19:06:35 -0500

    completed

--- main.js
+++ main.js
@@ -5,15 +5,47 @@
 //          PART ONE
 // ***************************
 // Write a JavaScript program to display the current day and time, start with:
-console.log(new Date)
-
 const displayDate = () => {
   const currentDate = new Date()
 
   document.getElementById("display-element").innerHTML = currentDate;
 }
- 
-
+const today = new Date();
+const day = today.getDay();
+const daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+console.log(`Today is : ${daylist[day]}.`);
+let hour = today.getHours();
+const minute = today.getMinutes();
+const second = today.getSeconds();
+let prepand = (hour >= 12)? " PM ":" AM ";
+hour = (hour >= 12)? hour - 12: hour;
+if (hour===0 && prepand===' PM ')
+{
+  if (minute===0 && second===0)
+  {
+    hour=12;
+    prepand=' Noon';
+  }
+  else
+  {
+    hour=12;
+    prepand=' PM';
+  }
+}
+if (hour===0 && prepand===' AM ')
+{
+  if (minute===0 && second===0)
+  {
+    hour=12;
+    prepand=' Midnight';
+  }
+  else
+  {
+    hour=12;
+    prepand=' AM';
+  }
+}
+console.log(`Current Time : ${hour}${prepand} : ${minute} : ${second}`);
 // Write a JavaScript program to convert a number to a string.
 
 
